f359858913792a25f086258fc0c2b0c7a5723727
suchanun6104101393 <suchanun0209@gmail.com>
2020-09-28 21:12:59 +0700

    393

---
+++ pages/product/_id.vue
@@ -0,0 +1,21 @@
+<template>
+  <div> <v-card-text>
+      <p class="display-1 text--primary">
+        {{ this.$route.params.id.data[0].title }}
+      </p>
+    </v-card-text>
+    <v-card
+    class="mx-auto"
+    color="teal"
+    width="2000px"
+  >
+    <v-card-text>
+    Description : {{ this.$route.params.id.data[0].description }} <br><br>
+    Nasa_id : {{ this.$route.params.id.data[0].nasa_id }} <br><br>
+    Center : {{ this.$route.params.id.data[0].center }} <br><br>
+    Date_created : {{ this.$route.params.id.data[0].date_created }} <br><br>
+    Keywords : {{ this.$route.params.id.data[0].keywords }} <br><br>
+    </v-card-text>
+  </v-card>
+  </div>
+</template>
